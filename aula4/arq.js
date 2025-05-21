9551b26e215b63fc73ff5d973c2a4d32c115be01
WasabyDev <kepoxander@gmail.com>
2025-05-21 10:12:38 -0300

    chore: understanding variables, use of signals, switch and for

---
+++ aula4/arq.js
@@ -0,0 +1,52 @@
+//primeiros passos com javascript
+
+//var
+//let
+//const
+
+//var olamundo = "Olá Mundo";
+//console.log(olamundo);
+
+//let a = 5;
+//let b = "10";
+//console.log(a == b || typeof a == b 'string');
+
+//let cor = "verde";
+
+//if (cor === verde) {
+//  console.log("siga");
+//  } else if (cor === amerelo){
+//    console.log("atenção");
+//  }
+// } else {
+//  console.log("pare");
+// }
+
+//= atribuição
+//== comparação
+//=== comparação de um valor e o tipo de dado
+
+let cor = verde;
+
+switch (cor) {
+  case "amarelo":
+    console.log("atenção");
+    break;
+
+  case "vermelho":
+    console.log("pare");
+    break;
+
+  case "verde":
+    console.log("siga");
+    break;
+
+  default:
+    console.log("nao sei");
+}
+
+let = 5;
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i} X ${n} = ${i * n}`);
+}
